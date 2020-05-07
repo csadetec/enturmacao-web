554e3c08f8d2957dbc250962b2235aa6dfcd0b29
Csa Detec <informaticact@santoagostinho.com.br>
2020-05-07 20:01:24 +0000

    config api gitpod

--- src/service/api.js
+++ src/service/api.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const hostname = () => {
   const app = window.location.hostname 
+  if(app === '3000-b1fb3da5-de03-4e7b-a5ce-cd8d56453343.ws-us02.gitpod.io')
+    return 'https://8000-b9d5fcf3-bc62-4975-8503-3f846f8906ae.ws-us02.gitpod.io'
   if(app === '10.20.0.22')
     return 'http://10.20.2.22:8000'
   
